a03aae3051322038e3d4bc33cf7f2fc6ffba34f8
f6ng <D0omS1ayer@yandex.ru>
2025-05-07 20:15:17 +0400

    Add files via upload

---
+++ Задание 6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Практика2_Задание6
+{
+    class Program
+    {
+        static void Main()
+        {
+            float a = 3.2f;
+            decimal b = 2.6m;
+            double c = 4.5;
+
+            // Приводим все к типу double явно:
+            c = (double)a    // преобразуем float → double
+              + (double)b    // преобразуем decimal → double
+              + c;           // прибавляем исходное c
+
+            Console.WriteLine($"Сумма a + b + c = {c}");
+        }
+    }
+}
