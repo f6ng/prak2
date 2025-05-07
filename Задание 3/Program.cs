2d0e965e31cb6d3bcaef7ecf861db59380d5bc10
f6ng <D0omS1ayer@yandex.ru>
2025-05-07 20:13:39 +0400

    Add files via upload

---
+++ Задание 3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Практика2_Задание3
+{
+    class Program
+    {
+        static void Main()
+        {
+            // Создаём генератор случайных чисел
+            Random rnd = new Random();
+
+            // Выводим случайное число от 0 до Int32.MaxValue
+            Console.WriteLine(rnd.Next());
+
+            // Выводим случайное число от 0 до 4
+            Console.WriteLine(rnd.Next(5));
+
+            // Выводим случайное число от -5 до 4
+            Console.WriteLine(rnd.Next(-5, 5));
+        }
+    }
+}
