c933d7eebf9600bde39242902caa9b468220fad9
f6ng <D0omS1ayer@yandex.ru>
2025-05-07 20:14:38 +0400

    Add files via upload

---
+++ Задание 4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Практика2_Задание7
+{
+    class Program
+    {
+        static void Main()
+        {
+            int a = 3;
+            double b = 2.6;
+
+            // При вычислении a неявно приводится к double,
+            // и результат (double) записывается в переменную b
+            b = a + b;
+
+            Console.WriteLine($"Сумма a + b = {b}");
+        }
+    }
+}
